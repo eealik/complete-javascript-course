69796d0228f6238ad5d7d2b7c76b2cb1e840f3ef
eealik <eeali@avaya.com>
2024-12-20 18:58:19 -0500

    Create script.js

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,55 @@
+/*
+let js = 'amazing';
+if (js === 'amazing') alert('Java is Fun');
+
+let value = (2 + 44 + 7 - 1);
+console.log(value);
+*//*var job = "consultant"
+job = 'architect'
+console.log(job)
+console.log(17 - 2 - 4)
+*/
+
+// const age = 19;
+// if (age >= 18) {
+//     console.log('My frined is really old ❤️')
+// } else { console.log('not old') }
+
+// const favourate = Number(prompt('what is your age'))
+// console.log(favourate)
+// console.log(typeof favourate)
+
+// const hasDriverLic = true;
+// const hasGoodVision = true;
+// const isTired = false;
+// console.log(hasDriverLic && hasGoodVision)
+// console.log(hasDriverLic || hasGoodVision)
+
+// if (hasDriverLic && hasDriverLic && !isTired) {
+//     console.log(`sarah can drive`)
+// }
+// else {
+//     console.log(`sara cannot drive`)
+// }
+
+// const hasDriverLic = true;
+// const hasGoodVision = true;
+// const isTired = false;
+
+// switch (hasDriverLic && hasDriverLic && !isTired) {
+//     case true:
+//         console.log(`sarah can drive`);
+//         break;
+//     case false:
+//         console.log(`sara cannot drive`)
+//         break;
+//     default:
+// //         console.log(`retry`)
+// // }
+// let age = Number(prompt(`what is your age?`));
+// age >= 50 ? console.log('you are old enough') : console.log(`you are young`)
+
+/* Write your code below. Good luck! 🙂 */
+const bill = 435;
+const tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
+console.log(`the bill was ${bill}, and the tip was ${tip}, and the total was ${bill + tip}`)
