edf3e1ce219e654c0919a356031bb1fe9265813c
eealik <eeali@avaya.com>
2024-12-20 18:57:19 -0500

    Update script.js

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,20 @@
+'use strict';
+// function logger() {
+//     console.log(`My name is Elsadig`)
+// }
+// // calling / running or invoking fucntions
+// logger();
+// logger();
+// logger();
+
+
+function fruitProcessor(apples, oranegs) {
+    console.log(apples, oranegs);
+    const juice = `Juice with ${apples} apples and ${oranegs} oranges.`
+    return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
